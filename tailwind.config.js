f63d2aa9a7c93693925eac9cdde2b0f5a8ee5b50
ShreyanshPachouri <165273155+ShreyanshPachouri@users.noreply.github.com>
2024-07-17 16:14:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
